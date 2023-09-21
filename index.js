8d1e6d30e811325bd5a8e577f39c3a239e32342c
saipranathi13 <87488120+saipranathi13@users.noreply.github.com>
2023-09-21 15:39:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const element = (
-//   Write your code here.
+<div className="main-image">
+      <h1>Super Over League</h1>
+      <div className="d-flex flex-row justify-content-start">
+          <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+          <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"/>
+      </div>
+  </div>
 )
 
 ReactDOM.render(element, document.getElementById('root'))
